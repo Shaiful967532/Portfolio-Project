8a86126d27339b203a24ca3ff20e34de87961a50
Shaiful Islam Emon <eshaifulislam@gmail.com>
2025-11-25 23:48:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// navbar scroll effect
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
